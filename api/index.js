7f7a8f6c8b98518e494fcb1bf4e30f4b735aa983
Ali Sadri <asadri721@gmail.com>
2021-07-25 15:21:25 +0430

    routes and models files

--- api/index.js
+++ api/index.js
@@ -1,6 +1,19 @@
 const express = require('express');
-const app = express()
+const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
-app.listen("8000", ()=>{
-    console.log('Server is running ...');
-})
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(console.log('Database Connected ...'))
+  .catch((err) => console.log(err));
+
+app.listen('8000', () => {
+  console.log('Server is running ...');
+});
