72fc800186db9ca405de16e29add39760e782cce
Ali Sadri <asadri721@gmail.com>
2021-08-08 16:36:08 +0430

    Register page and Routing

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,34 @@ import Single from './components/pages/single/Single';
 import Write from './components/pages/write/Write';
 import Settings from './components/pages/settings/Settings';
 import Login from './components/pages/login/Login';
+import Register from './components/pages/register/Register';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <>
+    <Router>
       <TopBar />
-      <Login />
-    </>
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route path='/register'>
+          <Register />
+        </Route>
+        <Route path='/login'>
+          <Login />
+        </Route>
+        <Route path='/write'>
+          <Write />
+        </Route>
+        <Route path='/settings'>
+          <Settings />
+        </Route>
+        <Route path='/post/:postId'>
+          <Single />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
